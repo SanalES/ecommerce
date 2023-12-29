6133f1988e9c4e71b9cb7a4a1168296f48625a3d
Sanal <130902573+SanalES@users.noreply.github.com>
2023-12-29 19:47:35 +0530

    changes 1

--- lib/features/personalization/screens/cart/cart.dart
+++ lib/features/personalization/screens/cart/cart.dart
@@ -1,12 +1,7 @@
 import 'package:ecommerce/common/widgets/appbar/appbar.dart';
 import 'package:ecommerce/common/widgets/icons/t_circular_icon.dart';
-import 'package:ecommerce/common/widgets/images/t_rounded_image.dart';
-import 'package:ecommerce/common/widgets/products.cart/cart_menu_icon.dart';
 import 'package:ecommerce/common/widgets/products/cart/cart_item.dart';
-import 'package:ecommerce/common/widgets/texts/product_title_text.dart';
-import 'package:ecommerce/common/widgets/texts/t_brand_title_text_with_verified_icon.dart';
 import 'package:ecommerce/utils/constants/colors.dart';
-import 'package:ecommerce/utils/constants/image_strings.dart';
 import 'package:ecommerce/utils/constants/sizes.dart';
 import 'package:ecommerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
@@ -30,17 +25,39 @@ class CartScreen extends StatelessWidget {
                 CartItem(),
                   Row(
                     children: [
+                      const SizedBox(width:70 ,),
                       
                       ///Add and remove button
                       ///
-                      TCircularIcon(
-                        icon:Iconsax.minus,
-                      height: 32,
-                      width: 32,
-                      size: Sizes.md,
-                      color: THelperFunctions.isDarkMode(context) ? Colors.white : Colors.black,),
+                      Row(
+                        children: [
+                          TCircularIcon(
+                            icon:Iconsax.minus,
+                          height: 32,
+                          width: 32,
+                          size: Sizes.md,
+                          color: THelperFunctions.isDarkMode(context) ? Colors.white : Colors.black,
+                          backgroundColor:THelperFunctions.isDarkMode(context) ? TColors.darkerGrey: TColors.light,
+                          ),
+
+                          const SizedBox(width: Sizes.spaceBtwItems,),
+                          Text("2" , style: Theme.of(context).textTheme.titleSmall,),
+                          const SizedBox(width: Sizes.spaceBtwItems,),
+                          TCircularIcon(
+                            icon:Iconsax.add,
+                            height: 32,
+                            width: 32,
+                            size: Sizes.md,
+                            color:  Colors.white,
+                            backgroundColor: TColors.primary,
+                          ),
+                        ],
+                      ),
+
+
+
                     ],
-                  )
+                  ),
 
                    ],
                  )
