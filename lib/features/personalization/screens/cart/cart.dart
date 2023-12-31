8fe272c739505151655fdc388fe37bc48bbae849
Sanal <130902573+SanalES@users.noreply.github.com>
2023-12-31 17:39:42 +0530

    changes 1

--- lib/features/personalization/screens/cart/cart.dart
+++ lib/features/personalization/screens/cart/cart.dart
@@ -1,7 +1,9 @@
 import 'package:ecommerce/common/widgets/appbar/appbar.dart';
 import 'package:ecommerce/features/personalization/screens/cart/widgets/tCart_items.dart';
+import 'package:ecommerce/features/personalization/screens/checkout/CheckoutScreen.dart';
 import 'package:ecommerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -16,7 +18,7 @@ class CartScreen extends StatelessWidget {
             ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(Sizes.defaultSpace),
-        child: ElevatedButton(onPressed: () {},child: const Text("Checkout \$256"),),
+        child: ElevatedButton(onPressed: () => Get.to(() => const  CheckoutScreen()),child: const Text("Checkout \$256"),),
       ),
 
     );
--- lib/features/personalization/screens/checkout/CheckoutScreen.dart
+++ lib/features/personalization/screens/checkout/CheckoutScreen.dart
@@ -1,3 +1,9 @@
+import 'package:ecommerce/common/widgets/appbar/appbar.dart';
+import 'package:ecommerce/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:ecommerce/features/personalization/screens/cart/widgets/tCart_items.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:ecommerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
 class CheckoutScreen extends StatelessWidget {
@@ -5,6 +11,39 @@ class CheckoutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold());
+    final dark =THelperFunctions.isDarkMode(context);
+    return Scaffold(
+      appBar: TAppBar(title: Text("Orders" ,style: Theme.of(context).textTheme.headlineSmall, ),showBackArrow: true,),
+  body: SingleChildScrollView(
+    child: Padding(padding: EdgeInsets.all(Sizes.defaultSpace),
+    child: Column(
+      children: [
+
+         ///  -------Cartile Items Kanikan
+        const TCartItem(showAddRemoveButtons: false),
+        const SizedBox(height: Sizes.spaceBtwSections,),
+
+        ///Coupon , Offers
+        TRoundedContainer(
+         showBorder: true,
+          backgroundColor:  dark ? TColors.dark : Colors.white,
+          padding: EdgeInsets.only(top:Sizes.sm , right: Sizes.sm , left: Sizes.md ),
+          child: Row(
+            children: [
+              Flexible(
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Have a coupon code? Enter Here"
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+
+      ],
+    ),),
+  ),
+    );
   }
 }
